616057a0af978fbdf707ca36cfedcd4c37a20833
jay <youngjin2712@naver.com>
2025-10-31 12:50:07 +0900

    연산자 시작

--- 01_var/basic.js
+++ 01_var/basic.js
@@ -1,1 +1,18 @@
 console.log('자바스크립트 시작');
+// alert('경고');
+// confirm('저장하시겠습니까?');
+// const name = prompt('이름 입력', '');
+document.writeln(name);
+
+let myName = '조영진';  // 문자열
+let myPoint = 123;  // 숫자
+let isAdult = true;  // boolean
+let myHobbies = ['독서', '여행', '요리']  // 배열
+let myAssets = {
+  car : 'sonata',
+  house : '아파트',
+  cash : 10000000
+}  // 객체
+let myFamily;  // undefined 변수 -> null : object
+
+console.log(typeof myFamily);
---
+++ 01_var/basic2.js
@@ -0,0 +1,38 @@
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log('연산결과 : ' + (num1 +num2));
+
+let str1 = 'Hello, ';
+let str2 = 'world';
+console.log(str1 + str2);
+
+let res = '';
+for (let i = 0; i < 5; i++) {
+  res += str1;
+}
+res += str2;
+console.log(res);
+
+let temp1 = prompt('숫자값 입력');
+let temp2 = prompt('숫자값 입력');
+console.log(temp1 == temp2);
+
+// let a = Number(prompt('값 입력'));
+// let b = Number(prompt('값 입력'));
+// let ab = a + b;
+// console.log(ab);
+
+// let answer = Math.floor(Math.random() * 100);
+// while (true) {
+//   let user = Number(prompt('숫자 입력'));
+//   if (user < answer) {
+//     alert('업!');
+//   } else if (user > answer) {
+//     alert('다운!');
+//   } else {
+//     alert('정답!');
+//     break;
+//   }
+// }
+
