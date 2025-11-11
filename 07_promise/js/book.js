4abe381c0a9b779985091530e07eccbaa58acac5
jay <youngjin2712@naver.com>
2025-11-11 17:51:41 +0900

    1111

--- 07_promise/js/book.js
+++ 07_promise/js/book.js
@@ -1,4 +1,4 @@
-const bookList = [
+let bookList = [
   {
     bookNo: "001",
     title: "모던 자바스크립트 기초",
@@ -13,13 +13,28 @@ const bookList = [
   },
 ];
 
+// 리스트 불러오기
 const showList = () => {
   const tbody = document.querySelector("tbody");
   tbody.innerHTML = "";
   bookList.forEach(({ bookNo, title, author, price }) => {
     let tr = document.createElement("tr");
     let td = document.createElement("td");
-    td.innerHTML = "<input type='checkbox' >";
+    let chkBox = document.createElement("input");
+    chkBox.type = "checkbox";
+    chkBox.addEventListener("click", (e) => {
+      let chkList = document.querySelectorAll("tbody input[type='checkbox']");
+      let flag = [];
+      chkList.forEach((elem) => {
+        flag.push(elem.checked);
+      });
+      if (flag.every((e) => e === true)) {
+        document.querySelector("#checkAll").checked = true;
+      } else {
+        document.querySelector("#checkAll").checked = false;
+      }
+    });
+    td.appendChild(chkBox);
     tr.appendChild(td);
     let data = [bookNo, title, author, price];
     for (let d of data) {
@@ -32,7 +47,8 @@ const showList = () => {
 };
 showList();
 
-let button = document.querySelector("button").addEventListener("click", (e) => {
+// 등록버튼
+document.querySelector("#add").addEventListener("click", (e) => {
   e.preventDefault();
   let bookNo = document.getElementById("bookNo").value;
   let title = document.getElementById("title").value;
@@ -51,14 +67,25 @@ let button = document.querySelector("button").addEventListener("click", (e) => {
   showList();
 });
 
-document.querySelector("#checkAll").addEventListener("click", (e) => {
-  if (e.target.checked) {
-    document
-      .querySelectorAll("tbody input")
-      .forEach((elem) => (elem.checked = true));
-  } else {
-    document
-      .querySelectorAll("tbody input")
-      .forEach((elem) => (elem.checked = false));
-  }
+// 체크박스
+document.querySelector("#checkAll").addEventListener("change", (e) => {
+  document
+    .querySelectorAll("tbody input[type='checkbox']")
+    .forEach((elem) => (elem.checked = e.target.checked));
 });
+
+// 선택삭제
+document.querySelector("#del").addEventListener("click", (e) => {
+  let checkedList = document.querySelectorAll("tbody input[type='checkbox']");
+  let newList = [];
+  for (let i = 0; i < checkedList.length; i++) {
+    if (!checkedList[i].checked) {
+      newList.push(bookList[i]);
+    }
+  }
+  bookList = newList;
+  showList();
+});
+
+// 체크박스 전체 선택
+document.querySelector("tbody input[type='checkbox']");
